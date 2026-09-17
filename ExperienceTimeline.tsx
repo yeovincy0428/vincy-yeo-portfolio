b043f8e9b0bf421eb8b2b95d8967ecb0cb14e146
yeovincy0428 <yeovincy0428@gmail.com>
2026-09-17 11:51:49 +0800

    Update ExperienceTimeline.tsx

--- ExperienceTimeline.tsx
+++ ExperienceTimeline.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { EXPERIENCES, EDUCATION } from '../data/portfolioData';
+import { EXPERIENCES, EDUCATION } from './portfolioData';
 import { Briefcase, GraduationCap, MapPin, Award, Calendar, CheckCircle2 } from 'lucide-react';
 import { WashiTape, StampSeal } from './HandDrawnSvg';
 
